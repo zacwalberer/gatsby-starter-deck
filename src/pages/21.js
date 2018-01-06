59e849fc74f9b9477a5e8c2a1ac8560a65d30713
Zac Walberer <zac.walberer@icloud.com>
2018-01-06 10:20:14 -0700

    Typos

--- src/pages/21.js
+++ src/pages/21.js
@@ -5,6 +5,6 @@ export default ({ transition }) => (
   <div style={transition && transition.style}>
     <img src="https://i.imgur.com/9YPEijI.gif" alt="BB8" />
     <p>Desktop experience <a href="https://invis.io/QXB81HAKC" target="_blank">Invision</a> 🖥</p>
-    <p>Mobile experience <a href="https://invis.io/CYAZ7PVJVs" target="_blank">Invision</a> 📱</p>
+    <p>Mobile experience <a href="https://invis.io/CYAZ7PVJV" target="_blank">Invision</a> 📱</p>
   </div>
 );
--- src/pages/22.js
+++ src/pages/22.js
@@ -6,7 +6,7 @@ export default ({ transition }) => (
     <h2>Reception</h2>
     <p>Propel announcement at ICON <a href="https://www.youtube.com/watch?v=mxSqBF9imxY" target="_blank">Youtube</a> 🎤</p>
     <p>Propel demo at ICON <a href="https://www.youtube.com/watch?v=-gm2QpuG_k8" target="_blank">Youtube</a> 💪</p>
-    <p>Infusionsoft Marketing <a href="https://www.infusionsoft.com/about/news/press-releases/infusionsoft-launches-new-products-simplify-growth-any-small-business" target="_blank">Invision</a> 💣</p>
+    <p>Infusionsoft announcement <a href="https://www.infusionsoft.com/about/news/press-releases/infusionsoft-launches-new-products-simplify-growth-any-small-business" target="_blank">Marketing</a> 💣</p>
     <p>Small Business Trends <a href="https://smallbiztrends.com/2017/04/infusionsoft-propel-supplies-mobile-marketing-experience.html" target="_blank">Article</a> 🤔</p>
   </div>
 );
